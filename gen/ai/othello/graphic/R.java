08acabcc18987a5489014f145ab6bf7e61eca570
Emanuele Crosato <crosato.emanuele@gmail.com>
2012-07-20 16:41:42 +0200

    istruzioni modificate

--- gen/ai/othello/graphic/R.java
+++ gen/ai/othello/graphic/R.java
@@ -20,9 +20,10 @@ public final class R {
     public static final class drawable {
         public static final int dark=0x7f020000;
         public static final int ic_launcher=0x7f020001;
-        public static final int light=0x7f020002;
-        public static final int othello=0x7f020003;
-        public static final int sfondo=0x7f020004;
+        public static final int iniziale=0x7f020002;
+        public static final int light=0x7f020003;
+        public static final int othello=0x7f020004;
+        public static final int sfondo=0x7f020005;
     }
     public static final class id {
         public static final int RelativeLayoutContent=0x7f060013;
@@ -31,26 +32,26 @@ public final class R {
         public static final int bntInstruction=0x7f060011;
         public static final int bntPlay=0x7f060010;
         public static final int gridview=0x7f060014;
+        public static final int howToPlayTitle=0x7f060006;
         public static final int imageDark=0x7f060017;
         public static final int imageLight=0x7f060018;
+        public static final int imageView1=0x7f060005;
         public static final int instructionTitle=0x7f060002;
+        public static final int introductionText1=0x7f060003;
+        public static final int introductionText2=0x7f060004;
         public static final int layoutRight=0x7f060015;
-        public static final int objectText=0x7f060004;
-        public static final int objectTitle=0x7f060003;
         public static final int orizontalLayout=0x7f060001;
         public static final int playerDarkCounter=0x7f06001a;
         public static final int playerLightCounter=0x7f06001b;
-        public static final int rulesText_1=0x7f060006;
-        public static final int rulesText_10=0x7f06000f;
-        public static final int rulesText_2=0x7f060007;
-        public static final int rulesText_3=0x7f060008;
-        public static final int rulesText_4=0x7f060009;
-        public static final int rulesText_5=0x7f06000a;
-        public static final int rulesText_6=0x7f06000b;
-        public static final int rulesText_7=0x7f06000c;
-        public static final int rulesText_8=0x7f06000d;
-        public static final int rulesText_9=0x7f06000e;
-        public static final int rulesTitle=0x7f060005;
+        public static final int rulesText_1=0x7f060007;
+        public static final int rulesText_2=0x7f060008;
+        public static final int rulesText_3=0x7f060009;
+        public static final int rulesText_4=0x7f06000a;
+        public static final int rulesText_5=0x7f06000b;
+        public static final int rulesText_6=0x7f06000c;
+        public static final int rulesText_7=0x7f06000d;
+        public static final int rulesText_8=0x7f06000e;
+        public static final int rulesText_9=0x7f06000f;
         public static final int scrollView1=0x7f060000;
         public static final int tableRow1=0x7f060019;
         public static final int tableRow2=0x7f060016;
@@ -61,22 +62,22 @@ public final class R {
         public static final int table_game=0x7f030002;
     }
     public static final class string {
-        public static final int alphabetaText=0x7f050016;
+        public static final int alphabetaText=0x7f050015;
         public static final int app_name=0x7f050001;
-        public static final int cancel=0x7f050017;
-        public static final int device_device=0x7f050019;
+        public static final int cancel=0x7f050016;
+        public static final int device_device=0x7f050018;
         public static final int exit=0x7f050003;
+        public static final int howToPlayTitle=0x7f05000a;
         public static final int instruction=0x7f050004;
         public static final int instructionTitle=0x7f050007;
-        public static final int minmaxText=0x7f050015;
-        public static final int objectText=0x7f050009;
-        public static final int objectTitle=0x7f050008;
+        public static final int introductionText1=0x7f050008;
+        public static final int introductionText2=0x7f050009;
+        public static final int minmaxText=0x7f050014;
         public static final int play=0x7f050002;
         public static final int playerDarkText=0x7f050005;
         public static final int playerLightText=0x7f050006;
-        public static final int player_device=0x7f050018;
+        public static final int player_device=0x7f050017;
         public static final int rulesText_1=0x7f05000b;
-        public static final int rulesText_10=0x7f050014;
         public static final int rulesText_2=0x7f05000c;
         public static final int rulesText_3=0x7f05000d;
         public static final int rulesText_4=0x7f05000e;
@@ -85,7 +86,6 @@ public final class R {
         public static final int rulesText_7=0x7f050011;
         public static final int rulesText_8=0x7f050012;
         public static final int rulesText_9=0x7f050013;
-        public static final int rulesTitle=0x7f05000a;
         public static final int title=0x7f050000;
     }
 }
